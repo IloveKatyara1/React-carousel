b6cd82b76f6afa43dcf6c56937cc7364877d2c6e
Nazar Tsventarnyi <110458518+IloveKatyara1@users.noreply.github.com>
2023-07-16 15:35:51 +0300

    added dots

--- src/Carousel.js
+++ src/Carousel.js
@@ -3,15 +3,31 @@ import './carouselComp.scss';
 
 const Carousel = ({ children, showSlides }) => {
     const sliderContent = useRef(null);
+    const dotsRef = useRef([]);
 
     const [currentSlide, setCurrentSlide] = useState(0);
     const [cantMove, setCantMove] = useState(false);
+    const [lastActiveDot, setLastActiveDot] = useState(0);
 
     const childrenSlides = React.Children.map(children, (child) => {
         return React.cloneElement(child, {
             className: child.props.className ? `${child.props.className} carouselComp_slide` : 'carouselComp_slide',
             style: { width: `${100 / showSlides}%` },
         });
+    });
+
+    const dots = childrenSlides.map((el, i) => {
+        if (i % showSlides === 0) {
+            return (
+                <div
+                    className="carouselComp__dot"
+                    ref={(ref) => dotsRef.current.push(ref)}
+                    key={i}
+                    onClick={() => setCurrentSlide(i)}>
+                    {i}
+                </div>
+            );
+        }
     });
 
     const fullChildrenList = [
@@ -44,11 +60,17 @@ const Carousel = ({ children, showSlides }) => {
             timeoutLastFirstSlide(childrenSlides.length - showSlides);
         } else if (currentSlide >= childrenSlides.length) {
             timeoutLastFirstSlide(0);
+        } else {
+            changeActiveDot(currentSlide);
+            setLastActiveDot(currentSlide / showSlides);
         }
     }, [currentSlide]);
 
     const timeoutLastFirstSlide = (frstLast) => {
         setCantMove(true);
+
+        changeActiveDot(frstLast);
+        setLastActiveDot(frstLast);
 
         setTimeout(() => {
             sliderContent.current.style.transition = '0s all';
@@ -63,6 +85,11 @@ const Carousel = ({ children, showSlides }) => {
                 sliderContent.current.style.transition = '1s all';
             }, 25);
         }, 975);
+    };
+
+    const changeActiveDot = (slide) => {
+        dotsRef.current[lastActiveDot].classList.remove('carouselComp__dot_active');
+        dotsRef.current[slide / showSlides].classList.add('carouselComp__dot_active');
     };
 
     console.log(currentSlide, 'currentSlide');
@@ -80,6 +107,7 @@ const Carousel = ({ children, showSlides }) => {
                     {fullChildrenList}
                 </div>
             </div>
+            <div className="carouselComp__dots_wrapper">{dots}</div>
             <button className="carouselComp__next" onClick={() => ChangeSlideByBtn(1)}>
                 next
             </button>
